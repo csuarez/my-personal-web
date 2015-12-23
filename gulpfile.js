e0bee0eca65d707d9ca3456a922d595b303c460f
César Suárez <suarez.ortega.cesar@gmail.com>
2015-12-23 21:04:00 +0100

    Soporte de Gulp+Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var sassInput = './stylesheets/**/*.scss';
+var sassOutput = './public/css';
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+gulp.task('default', ['sass']);
+
+
+gulp.task('sass', function () {
+  return gulp
+    .src(sassInput)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(sassOutput));
+});
+
+gulp.task('watch', function() {
+  return gulp
+    .watch(sassInput, ['sass'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
